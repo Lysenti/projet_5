873fd947613a434e55ed929b5f3e897f4e8f17d7
Lysenti <eren926@gmail.com>
2024-07-31 21:23:28 +0200

    "Animation du déroulement dans le menu About"

--- projet_5_app/src/components/Collapse.js
+++ projet_5_app/src/components/Collapse.js
@@ -1,20 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
 import './Collapse.scss';
 
 const Collapse = ({ title, content }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef(null);
+  const [height, setHeight] = useState("0px");
+
+  useEffect(() => {
+    setHeight(isOpen ? `${contentRef.current.scrollHeight}px` : "0px");
+  }, [isOpen]);
+
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div className="collapse">
-      <div className="collapse__header" onClick={() => setIsOpen(!isOpen)}>
+      <div className="collapse__header" onClick={toggleCollapse}>
         <h2>{title}</h2>
         <button className={`collapse__button ${isOpen ? 'open' : ''}`}>
           <FontAwesomeIcon icon={faAngleUp} className={`collapse__icon ${isOpen ? 'open' : ''}`} />
         </button>
       </div>
-      {isOpen && <div className="collapse__content">{content}</div>}
+      <div
+        ref={contentRef}
+        className={`collapse__content ${isOpen ? 'collapse__content--open' : ''}`}
+        style={{ height }}
+      >
+        <div className="collapse__text">
+          {content}
+        </div>
+      </div>
     </div>
   );
 };
