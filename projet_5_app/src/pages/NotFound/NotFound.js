2aad82006866d7c25d51662c9206ed162d90f59d
Lysenti <eren926@gmail.com>
2024-08-06 13:57:08 +0200

    "Mise à jour du responsive sur les différentes pages"

--- projet_5_app/src/pages/NotFound/NotFound.js
+++ projet_5_app/src/pages/NotFound/NotFound.js
@@ -8,7 +8,7 @@ const NotFound = () => {
       <div className="layout__main notfound-main">
         <div className="not-found-content">
           <h1>404</h1>
-          <p>Oups! La page que vous demandez n'existe pas.</p>
+          <p className="responsive-text">Oups! La page que <span className="line-break"></span> vous demandez n'existe pas.</p>
           <Link to="/" className="home-link">Retourner sur la page d'accueil</Link>
         </div>
       </div>
